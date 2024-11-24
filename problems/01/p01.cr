3b169578b54f8cd05da3da57683ddd3dae4546c3
Andrea Manzini <ilmanzo@gmail.com>
2024-11-24 16:46:59 +0100

    add problem statement

--- problems/01/p01.cr
+++ problems/01/p01.cr
@@ -1,3 +1,3 @@
 FILENAME="input.txt"
-input = File.read(FILENAME).lines.map &.to_i
-p input.each_cons(60).map(&.sum).map{|x| x//60}.count{|x| x<1500 || x>1600}
+input= File.read(FILENAME).lines.map &.to_i
+p input.each_cons(60).map{|x| x.sum//60}.count{|x| x<1500 || x>1600}
